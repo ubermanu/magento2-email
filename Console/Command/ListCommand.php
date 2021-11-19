2974831fed53c05d6650c6e5b5b59b69018ef2d7
ubermanu <manu@paca.happy-dev.fr>
2021-11-19 13:58:19 +0100

    add an argument to filter by template identifier

--- Console/Command/ListCommand.php
+++ Console/Command/ListCommand.php
@@ -4,6 +4,7 @@ namespace Ubermanu\Email\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -28,7 +29,13 @@ class ListCommand extends Command
     protected function configure()
     {
         $this->setName('email:list');
-        $this->setDescription('List all the available email templates.');
+        $this->setDescription('List all the available email templates');
+
+        $this->addArgument(
+            'filter',
+            InputArgument::OPTIONAL,
+            'Filter the list by template identifier'
+        );
 
         parent::configure();
     }
@@ -38,16 +45,18 @@ class ListCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $filter = $input->getArgument('filter');
+
         $table = new Table($output);
         $table->setHeaders(['Identifier', 'Title', 'Module']);
         $count = 0;
 
         foreach ($this->_emailConfig->getAvailableTemplates() as $template) {
-            $table->addRow([
-                $template['value'],
-                $template['label'],
-                $template['group']
-            ]);
+            // Filter by template identifier (if provided)
+            if ($filter && substr($template['value'], 0, strlen($filter)) !== $filter) {
+                continue;
+            }
+            $table->addRow([$template['value'], $template['label'], $template['group']]);
             $count++;
         }
 
